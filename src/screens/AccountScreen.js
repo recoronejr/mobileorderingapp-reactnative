41c4a0c3edcad4885d609d106bf27afdea7a580a
cook2c9 <cook2c9@mail.uc.edu>
2021-01-21 23:57:31 -0500

    Added Edit Account Screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -4,20 +4,16 @@ import { NavigationEvents } from 'react-navigation'
 
 import AuthenticationNavigation from '../navigation/AuthenticationNavigation';
 import GetUserInfo from '../components/UserInfo'
-import { SignOutButton } from '../components/ButtonComp';
+import { SignOutButton, EditButton } from '../components/ButtonComp';
 
 export default class AccountScreen extends React.Component {
-    constructor(props) {
-        super(props)
-    }
     render() {
-        <NavigationEvents onDidFocus={() => alert('Welcome')}/>
-        const { navigation: { navigate } } = this.props;
         return (
             <View style = {styles.container}>
                 <View style = {styles.mainLbl}>
                 <Text>Account Settings</Text>
-                <GetUserInfo />
+                    <GetUserInfo />
+                    <EditButton />
                 </View>
                 <View>
                     <SignOutButton />
