55e535a2369df0164bead77ad87124f4b7380e20
Reco Gunnels Jr <recogunnelsll@gmail.com>
2021-01-25 17:17:42 -0500

    add models

--- src/model/Order.js
+++ src/model/Order.js
@@ -1,9 +1,21 @@
+import LinesItems from './LineItem';
+
 export default class Order {
     
-    let catalog: string;
-    
+    let location: string;
+    let lineItems[] = new LinesItems();
 
     constructor() {
         
+    }
+    addLineItem(item){
+      lineItems.push(item);
+    }
+    deleteLineItem(index){
+      lineItems.splice(index,1);
+    }
+    // Call based on location store
+    completeOrder(){
+
     }
   }
