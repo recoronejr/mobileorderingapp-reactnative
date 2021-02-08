d15fa72650f74039a196d9ac19d54de06c591443
cook2c9 <cook2c9@mail.uc.edu>
2021-02-08 16:33:59 -0500

    Fixing Account Screen Issue

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import * as firebase from 'firebase';
-import firestore from 'firestore';
+import '@firebase/firestore';
 
 import { View, ListItem, Text } from 'react-native'
 
@@ -82,7 +82,7 @@ class FirebaseAPI {
       return false;
     })           
   }
-  // Function that updates First Name, Last Name and Phone Numnber 
+  // Functions that update First Name, Last Name and Phone Numnber 
   updateFirstName(firstName) {
     let user = this.getUser();
     const userDocument = firebase.firestore()
